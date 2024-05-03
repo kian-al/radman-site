8c95f3823e8fa3882b6bc7c908db1b675ad6d664
Kian <kianalmasi0@gmail.com>
2024-05-04 01:47:36 +0330

    Template changes applied to the view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_view(request):
-    return HttpResponse('<h1>this is homme page</h1>')
+    return render(request,'index.html')
 def about_view(request):
-    return HttpResponse('<h1>this is about page</h1>')
+    return render(request,'about.html')
 def contacts_view(request):
-    return HttpResponse('<h1>this is contacts page</h1>')
+    return render(request,'contacts.html')
