c8cac49aa6f9dd4f18d71ac1037f59dd52d13068
Kian <kianalmasi0@gmail.com>
2024-05-04 21:23:46 +0330

    created general templates dir to project and deleted templates dir on blog dir

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_view(request):
-    return render(request,'index.html')
+    return render(request,'blog/index.html')
 def about_view(request):
-    return render(request,'about.html')
+    return render(request,'blog/about.html')
 def contacts_view(request):
-    return render(request,'contacts.html')
+    return render(request,'blog/contacts.html')
