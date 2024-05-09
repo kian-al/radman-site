4ced964f0dba57f1ee9d8dccc7807dc42517c90f
Kian <kianalmasi0@gmail.com>
2024-05-09 04:20:10 +0330

    linked template to the project

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from blog.views import *
 urlpatterns = [
     path('',index_view),
-    path('about/',about_view),
-    path('contacts/',contacts_view)
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_view(request):
-    return render(request,'blog/index.html')
-def about_view(request):
-    return render(request,'blog/about.html')
-def contacts_view(request):
-    return render(request,'blog/contacts.html')
+    return render(request, 'blog/index.html',)
+
