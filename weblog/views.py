229e38b42907dc9e09fdcca594057c569e668c07
Kian <kianalmasi0@gmail.com>
2024-07-23 00:05:19 +0330

    update weblog create and config search bar and slidbar weblog page

--- weblog/views.py
+++ weblog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from weblog.models import post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 def blog_view (request,**kwargs):
     posts=post.objects.filter(status=1)
     if kwargs.get('cat_name')  != None:
         posts=posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
-        posts=posts.filter(author__username=kwargs['author_username'])   
+        posts=posts.filter(author__username=kwargs['author_username'])  
+        
+    posts=Paginator(posts,5)
+    try:
+        page_number=request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts=posts.get_page(1)
+    except EmptyPage:
+        posts=posts.get_page(1)
+
     context={'posts':posts}
     return render(request,'blog/blog.html',context)
 
